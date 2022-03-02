751392e97b7d6ed1785f201b054aaf60a134a79b
lumaghg <rotkehlchen567@gmail.com>
2022-03-02 10:05:21 +0100

    isolated the crawl results

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"lumaghg/dualis-crawler/crawler"
-	"lumaghg/dualis-crawler/database"
-	"lumaghg/dualis-crawler/email"
 	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
@@ -16,27 +15,37 @@ type MyEvent struct {
 	NotificationEmail string `json:"notificationEmail"`
 }
 
-func HandleRequest(event MyEvent) error {
+func HandleRequest(event MyEvent) (string, error) {
 	start := time.Now()
 
 	results, _ := crawler.GetDualisCrawlResults(event.Email, event.Password)
 	fmt.Println(time.Since(start))
-	dualisChanges, err := database.UpdateDatabaseAndGetChanges(results, event.Email)
-	fmt.Println(dualisChanges)
+	fmt.Println(results)
+	resultsBytes, err := json.Marshal(results)
 	if err != nil {
-		fmt.Println(err)
-		return err
+		return "", err
 	}
-	if len(dualisChanges) > 0 {
-		err = email.SendUpdateEmail(dualisChanges, event.NotificationEmail)
-	}
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	return nil
+	return string(resultsBytes), nil
+
+	/*
+		dualisChanges, err := database.UpdateDatabaseAndGetChanges(results, event.Email)
+		fmt.Println(dualisChanges)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		if len(dualisChanges) > 0 {
+			err = email.SendUpdateEmail(dualisChanges, event.NotificationEmail)
+		}
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		return nil
+	*/
 }
 
 func main() {
+	//HandleRequest(MyEvent{Email: "s201808@student.dhbw-mannheim.de", Password: "xj3ghgPUx"})
 	lambda.Start(HandleRequest)
 }
